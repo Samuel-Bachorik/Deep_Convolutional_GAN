c701f6c4315ebbba3318a56e5ff6a1a22bb99e74
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2022-03-03 20:17:00 +0100

    Update graphing_class.py

--- graphing_class.py
+++ graphing_class.py
@@ -5,18 +5,30 @@ class CreateGraph:
     def __init__(self, batch_count,name):
 
         self.batch_count = batch_count
-        self.num_for_avg = 0
-        self.array_epoch, self.array_loss =  [],[]
+        self.num_for_G = 0
+        self.num_for_D = 0
+        self.array_epoch, self.array_loss_G, self.array_loss_D = [], [], []
         self.name = name
 
     def count(self, epoch):
 
         self.array_epoch.append(epoch)
-        self.array_loss.append(self.num_for_avg/ self.batch_count)
-        print(self.num_for_avg / self.batch_count, "Epoch avg " + self.name)
-        self.num_for_avg = 0
 
-        plt.plot(self.array_epoch, self.array_loss)
+        self.array_loss_G.append(self.num_for_G / self.batch_count)
+        self.array_loss_D.append(self.num_for_D / self.batch_count)
+
+        #print("Epoch average loss: Generator - {}, Discriminator - {}".format(self.num_for_G / self.batch_count, self.num_for_D / self.batch_count))
+
+        self.num_for_G = 0
+        self.num_for_D = 0
+
+
+        plt.plot(self.array_epoch, self.array_loss_G)
+        plt.plot(self.array_epoch, self.array_loss_D)
+
         plt.title(self.name)
-        plt.savefig(self.name+".png")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.legend(["Generator loss", "Discriminator loss"])
+        plt.savefig(self.name + ".png")
         plt.show()
